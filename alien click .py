1ef85039dbb78342a1cf4917252346a5ccc4eb78
Troy200 <kuriyanthomas27@gmail.com>
2025-02-05 17:14:55 +0100

    Add files via upload

--- alien click .py
+++ alien click .py
@@ -9,21 +9,37 @@ Alien= Actor("alien")
 Alien.x=(250)
 Alien.y=(250)
 msg=""
+x="light blue"
+score=0
+faults=0
 def draw():
-    screen.fill("light blue")
+    screen.fill(x)
     Alien.draw()
     
     screen.draw.text("Shoot the Alien",(200,10))
-    screen.draw.text(msg,(10,480),fontsize=50,color="black")
-
+    screen.draw.text(msg,(10,480),fontsize=30,color="black")
+    screen.draw.text("Score "+str(score),(10,460))
+    screen.draw.text("Faults "+str(faults),(10,440))
+        
 
 def on_mouse_down(pos):
-    global msg
+    global msg,x,score,faults
     if Alien.collidepoint(pos):
         Alien.x=random.randint(50,450)
         Alien.y=random.randint(50,450)
         msg="good shot"
-         #screen.fill("green")
+        x="green"
+        score=score+1
+
+       
     else:
         msg="you missed"
+        x="red"
+        faults=faults+1
+       
+
+
+
+
+
 pgzrun.go()
